4064702ed59e8ee6254752d7d13f0d5d89134322
nikolaipaukkonen <72209408+nikolaipaukkonen@users.noreply.github.com>
2021-12-14 22:45:38 +0200

    Luotu docstring locus.py:lle

--- src/database/locus.py
+++ src/database/locus.py
@@ -1,7 +1,27 @@
 ''' Luokka stratigrafisille yksiköille '''
 
 class Locus:
+    ''' Luokka, joka kuvaa stratigrafista kerrosta tai kontekstia (locusta).
+    
+    Attributes: 
+        l_type: Locuksen tyyppi (maakerros, rakenne tai leikkaus).
+        name: Locuksen uniikki nimi.
+        descr: Locuksen sanallinen kuvaus.
+        thick: Locuksen paksuus senttimetreinä.
+        above: Mahdollisen ylläolevan locuksen id.
+        '''
+    
     def __init__(self, l_type, name, descr, thick, above):
+        ''' Konstruktori, jolla luodaan uusi locus.
+    
+        Args: 
+            l_type: Locuksen tyyppi (maakerros, rakenne tai leikkaus).
+            name: Locuksen uniikki nimi.
+            descr: Locuksen sanallinen kuvaus.
+            thick: Locuksen paksuus senttimetreinä.
+            above: Mahdollisen ylläolevan locuksen id.
+        '''
+        
         self.l_type=l_type
         self.name=name
         self.descr=descr
