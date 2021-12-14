403ab1fa3b064a71610cdb143ee1ff2741dce3da
nikolaipaukkonen <72209408+nikolaipaukkonen@users.noreply.github.com>
2021-12-14 22:42:41 +0200

    Luotu docstring find.py:lle

--- src/database/find.py
+++ src/database/find.py
@@ -1,7 +1,27 @@
 ''' Luokka löydöille '''
 
 class Find:
+    ''' Luokka, joka kuvaa arkeologisesta kontekstista tehtyä löytöä.
+    
+    Attributes: 
+        find_type: Löydön tyyppi.
+        dating: Löydön ajoitus periodina.
+        descr: Löydön sanallinen kuvaus.
+        weight: Löydön paino grammoina.
+        locus: Stratigrafinen yksikkö (locus), jossa löytö on tehty.
+        '''
+    
     def __init__(self, find_type, dating, descr, weight, locus):
+        ''' Luokan konstruktori, jolla luodaan uusi löytö.
+        
+        Args:
+            find_type: Löydön tyyppi.
+            dating: Löydön ajoitus periodina.
+            descr: Löydön sanallinen kuvaus.
+            weight: Löydön paino grammoina.
+            locus: Stratigrafinen yksikkö (locus), jossa löytö on tehty.
+        '''
+        
         self.find_type=find_type
         self.dating=dating
         self.descr=descr
